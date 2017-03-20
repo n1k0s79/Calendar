3abcd2c5f10d107ae0d2d3cddb4b8d748dd0c043
Nikos Sandalidis <nikos@sandalidis.gr>
2017-03-20 10:45:44 +0200

    Added the binaries

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -59,7 +59,7 @@ Partial Class Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(462, 308)
+        Me.ClientSize = New System.Drawing.Size(496, 335)
         Me.Controls.Add(Me.Button1)
         Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.nmYear)
